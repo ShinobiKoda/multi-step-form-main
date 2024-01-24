d619e679ba2616461a479a0d7ff69f9671474891
Praise <sirp2804@gmail.com>
2024-01-24 17:43:43 +0100

    completed navigation for mobile

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+//===code to toggle between monthly and yearly plans====
+
 const toggle = document.querySelector(".toggle");
 const monthly = document.querySelector(".yearly_billing");
 const yearly = document.querySelector(".monthly_billing");
@@ -27,4 +29,79 @@ toggle.addEventListener("click", () => {
     yearText.classList.add('sub-color');
   }
 
+});
+
+//======== code to change sections ===========//
+
+// get all the buttons and link them to their respective sections
+const btn_one = document.getElementById('personal-info');
+const btn_two = document.getElementById('select-plans');
+const btn_three = document.getElementById('add-ons');
+const btn_four = document.getElementById('thanks');
+
+// grab the all the sections
+const personal_info = document.getElementById('personal__info');
+const add_ons = document.getElementById('add__ons');
+const finishing_up = document.getElementById('finishing__up');
+const thanks = document.getElementById('thanks__page');
+
+btn_one.onclick = () => {
+  if(personal_info.classList.contains('none')){
+    personal_info.classList.remove('none');
+    add_ons.classList.add('none');
+    thanks.classList.add('none');
+    finishing_up.classList.add('none');
+    console.log('clicked');
+  }
+}
+
+btn_two.onclick = () => {
+  if(add_ons.classList.contains('none')){
+    add_ons.classList.remove('none');
+    personal_info.classList.add('none');
+    thanks.classList.add('none');
+    finishing_up.classList.add("none");
+  }
+}
+
+btn_three.onclick = () => {
+  if(finishing_up.classList.contains('none')){
+    finishing_up.classList.remove('none');
+    personal_info.classList.add('none');
+    add_ons.classList.add('none');
+    thanks.classList.add('none');
+  }
+}
+
+btn_four.onclick = () =>{
+  if(thanks.classList.contains('none')){
+    thanks.classList.remove('none');
+    personal_info.classList.add('none');
+    add_ons.classList.add('none');
+    finishing_up.classList.add('none');
+  }
+}
+
+// === grab next page button =========//
+const next_page = document.querySelector('.next');
+next_page.addEventListener('click', ()=>{
+  if(add_ons.classList.contains('none') && finishing_up.classList.contains('none') && thanks.classList.contains('none')){
+    personal_info.classList.add('none');
+    add_ons.classList.remove('none');
+  }else if(personal_info.classList.contains('none') && finishing_up.classList.contains('none') && thanks.classList.contains('none')){
+    add_ons.classList.add('none');
+    finishing_up.classList.remove('none');
+  }else if (personal_info.classList.contains("none") &&
+    add_ons.classList.contains("none") &&
+    thanks.classList.contains("none")
+  ){
+    finishing_up.classList.add('none');
+    thanks.classList.remove('none');
+  }else if(personal_info.classList.contains("none") &&
+    add_ons.classList.contains("none") &&
+    finishing_up.classList.contains("none")
+  ){
+    thanks.classList.add('none');
+    personal_info.classList.remove('none');
+  }
 });
